497081477e86ce7b0dd6d72519f9e70418a53736
Nadia Konieczny <nadiakrakowski@gmail.com>
2022-03-10 13:39:44 -0600

    better struct

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("str-in");
 const submitBtn = document.getElementById("submit");
+const output = document.getElementById("out");
 
 function getString() {
   const str = inputBox.value;
@@ -7,6 +8,7 @@ function getString() {
   console.log(freqArr);
   const tree = makeTree(freqArr);
   console.log(tree);
+  output.innerHTML = tree;
 }
 
 function makeTree(arr) {
@@ -14,17 +16,16 @@ function makeTree(arr) {
   arr.forEach(item => tree.push(item));
   let newNode;
   while (tree.length > 1) {
-    newNode = [];
-    newNode.push(
-      tree[tree.length - 1][0].concat(tree[tree.length - 2][0]),
-      tree[tree.length - 1][1] + tree[tree.length - 2][1],
-      tree[tree.length - 2],
-      tree[tree.length - 1]
-    );
+    newNode = {
+      symbol: tree[tree.length - 1].symbol.concat(tree[tree.length - 2].symbol),
+      freq: tree[tree.length - 1].freq + tree[tree.length - 2].freq,
+      node1: tree[tree.length - 2],
+      node2: tree[tree.length - 1]
+    };
     tree.pop();
     tree.pop();
     tree.push(newNode);
-    tree.sort((a, b) => b[1] - a[1]);
+    tree.sort((a, b) => b.freq - a.freq);
   }
   return tree;
 }
@@ -35,9 +36,14 @@ function getFrequency(str) {
     {}
   );
 
-  const freqArr = Object.entries(freqs).sort((a, b) => b[1] - a[1]);
+  //const freqArr = Object.entries(freqs).sort((a, b) => b[1] - a[1]);
 
-  console.log(freqArr);
+  let freqArr = [];
+  Object.entries(freqs).map(item => {
+    freqArr.push({ symbol: item[0], freq: item[1] });
+  });
+  freqArr.sort((a, b) => b.freq - a.freq);
+
   return freqArr;
 }
 
