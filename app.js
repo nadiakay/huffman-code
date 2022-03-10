9fb1882c361a8df942d537e0a9fa2c4e976ac384
Nadia Konieczny <nadiakrakowski@gmail.com>
2022-03-10 16:02:16 -0600

    now with tree visualization

--- app.js
+++ app.js
@@ -3,12 +3,45 @@ const submitBtn = document.getElementById("submit");
 const output = document.getElementById("out");
 
 function getString() {
+  output.innerHTML = "";
   const str = inputBox.value;
   const freqArr = getFrequency(str);
-  console.log(freqArr);
   const tree = makeTree(freqArr);
-  console.log(tree);
-  output.innerHTML = tree;
+  output.appendChild(domTree(tree[0], str.length));
+}
+
+// make function to traverse tree and build binary dictionary
+
+// clean this up
+function domTree(tree, len) {
+  const list = document.createElement("ul");
+  const symbol = document.createElement("li");
+  const freq = document.createElement("li");
+
+  list.style.padding = "10px";
+  list.style.border = "solid 1px";
+  list.style.margin = "5px";
+  list.style.width = "fit-content";
+
+  symbol.innerHTML = `symbol: ${tree.symbol}`;
+  console.log(len);
+  freq.innerHTML = `probability: ${(tree.freq / len).toFixed(2)}`;
+  list.appendChild(symbol);
+  list.appendChild(freq);
+  if (tree.node1) {
+    const nodes = document.createElement("ul");
+    const node1 = document.createElement("li");
+    const node2 = document.createElement("li");
+
+    nodes.style.borderLeft = "dotted 1px";
+
+    node1.appendChild(domTree(tree.node1, len));
+    node2.appendChild(domTree(tree.node2, len));
+    nodes.appendChild(node1);
+    nodes.appendChild(node2);
+    list.appendChild(nodes);
+  }
+  return list;
 }
 
 function makeTree(arr) {
@@ -35,8 +68,6 @@ function getFrequency(str) {
     (freq, sym) => Object.assign(freq, { [sym]: (freq[sym] || 0) + 1 }),
     {}
   );
-
-  //const freqArr = Object.entries(freqs).sort((a, b) => b[1] - a[1]);
 
   let freqArr = [];
   Object.entries(freqs).map(item => {
